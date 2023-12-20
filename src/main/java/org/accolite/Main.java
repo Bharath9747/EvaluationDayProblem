a25e9577a7f2457773411f53492d38c940177208
Bharath9747 <152379838+Bharath9747@users.noreply.github.com>
2023-12-20 14:25:03 +0530

    Add files via upload

---
+++ src/main/java/org/accolite/Main.java
@@ -0,0 +1,97 @@
+package org.accolite;
+
+import org.accolite.entity.PlayerStatistics;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Set<PlayerStatistics> statisticsSet = getExcelData("C:\\Users\\bharath.m\\IdeaProjects\\EvaluationDayProblem\\src\\main\\resources\\Statistics.xlsx");
+
+        PlayerStatistics highestScorer = statisticsSet.stream().sorted(Comparator.comparing(PlayerStatistics::getHighestScore)).collect(Collectors.toList()).get(statisticsSet.size()-1);
+        System.out.println(highestScorer.getPlayerName()+" "+ highestScorer.getHighestScore());
+        System.out.println();
+        PlayerStatistics secondHighestScorer = statisticsSet.stream().sorted(Comparator.comparing(PlayerStatistics::getHighestScore)).collect(Collectors.toList()).get(statisticsSet.size()-2);
+        System.out.println(secondHighestScorer.getPlayerName()+" "+secondHighestScorer.getHighestScore());
+        System.out.println();
+        PlayerStatistics leastScorer = statisticsSet.stream().sorted(Comparator.comparing(PlayerStatistics::getHighestScore)).findFirst().get();
+        System.out.println(leastScorer.getPlayerName()+" "+leastScorer.getHighestScore());
+        System.out.println();
+        PlayerStatistics oldestPlayer = statisticsSet.stream().sorted(Comparator.comparing(PlayerStatistics::getBirthDate)).collect(Collectors.toList()).get(0);
+        System.out.println(oldestPlayer.getPlayerName()+" "+ oldestPlayer.getBirthDate());
+        Map<Integer, List<PlayerStatistics>> groupPlayers = statisticsSet.stream().collect(Collectors.groupingBy(playerStatistics -> playerStatistics.getBirthDate().getYear() / 10, Collectors.filtering(p -> p.getBirthDate().getYear() / 10 >=0, Collectors.toList())));
+        groupPlayers.forEach(
+                (x,y)->{
+                    System.out.println(x*10+"'s");
+                    y.stream().forEach(
+                            xy-> System.out.println(xy.getPlayerName()+" "+xy.getBirthDate())
+                    );
+                }
+        );
+        System.out.println();
+        System.out.println("Top Scores On Decades");
+        groupPlayers.forEach(
+                (x,y)->{
+                    System.out.println(x*10+"'s");
+                    System.out.println(y.stream().sorted(Comparator.comparing(PlayerStatistics::getScores)).collect(Collectors.toList()).get(y.size()-1));
+                }
+        );
+        System.out.println();
+        System.out.println("Highest Strike Rate");
+        System.out.println(statisticsSet.stream().sorted(Comparator.comparing(PlayerStatistics::getStrikeRate)).collect(Collectors.toList()).get(statisticsSet.size()-1));
+        System.out.println();
+
+        statisticsSet.stream().forEach(
+                x-> System.out.println(x.getPlayerName()+" "+x.getWicketTaken()+" "+x.getMatchesPlayed()+" "+((float)x.getWicketTaken()/x.getMatchesPlayed()))
+        );
+        System.out.println();
+        System.out.println("Highest Average Wicket Taker ");
+
+        System.out.println(statisticsSet.stream().sorted(Comparator.comparing(playerStatistics -> playerStatistics.getWicketTaken()/ playerStatistics.getMatchesPlayed())).collect(Collectors.toList()).get(statisticsSet.size()-1));
+        System.out.println();
+
+        System.out.println("Indian Batsman");
+        statisticsSet.stream().filter(playerStatistics -> playerStatistics.getCountryName().equals("INDIA")).forEach(
+                x-> System.out.print(x.getPlayerName()+"\t")
+        );
+        System.out.println();
+
+
+    }
+    public static Set<PlayerStatistics> getExcelData(String fileName){
+        Set<PlayerStatistics> statisticsSet = new LinkedHashSet<>();
+        try {
+            FileInputStream inputStream = new FileInputStream(fileName);
+            Workbook workbook = new XSSFWorkbook(inputStream);
+            Sheet sheet = workbook.getSheetAt(0);
+            int rowCount = sheet.getPhysicalNumberOfRows();
+            for (int i = 1; i < rowCount; i++) {
+                Row row = sheet.getRow(i);
+                PlayerStatistics statistics = new PlayerStatistics();
+                statistics.setPlayerName(row.getCell(0).getStringCellValue().toUpperCase());
+                statistics.setCountryName(row.getCell(1).getStringCellValue().toUpperCase());
+                statistics.setBirthDate(row.getCell(2).getDateCellValue());
+                statistics.setScores((long) row.getCell(3).getNumericCellValue());
+                statistics.setMatchesPlayed((int) row.getCell(4).getNumericCellValue());
+                statistics.setWicketTaken((int) row.getCell(5).getNumericCellValue());
+                statistics.setHighestScore((int) row.getCell(6).getNumericCellValue());
+                statistics.setAverage( row.getCell(7).getNumericCellValue());
+                statistics.setStrikeRate(row.getCell(8).getNumericCellValue());
+                statisticsSet.add(statistics);
+            }
+
+        }
+        catch (Exception e)
+        {
+            e.printStackTrace();
+        }
+        return statisticsSet;
+    }
+}
---
+++ src/main/java/org/accolite/entity/PlayerStatistics.java
@@ -0,0 +1,25 @@
+package org.accolite.entity;
+
+import lombok.*;
+
+import java.util.Date;
+import java.util.Objects;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@ToString
+public class PlayerStatistics {
+    private String playerName;
+    private String countryName;
+    private Date birthDate;
+    private long scores;
+    private int matchesPlayed;
+    private int wicketTaken;
+    private int highestScore;
+    private double average;
+    private double strikeRate;
+
+
+}
